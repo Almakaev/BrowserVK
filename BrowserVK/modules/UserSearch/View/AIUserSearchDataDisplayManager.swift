a02f29eb10f22579f29385ccb2902b5a16824a7b
Ilyas Almakaev <i.almakaev@gmail.com>
2017-11-17 11:01:53 +0700

    first commit

---
+++ BrowserVK/modules/UserSearch/View/AIUserSearchDataDisplayManager.swift
@@ -0,0 +1,13 @@
+//
+//  AIUserSearchDataDisplayManager.swift
+//  BrowserVK
+//
+//  Created by Ильяс on 17.11.2017.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+import UIKit
+
+class AIUserSearchDataDisplayManager: NSObject {
+
+}
