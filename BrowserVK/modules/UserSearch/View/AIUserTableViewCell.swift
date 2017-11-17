7483e2b76cf51276f61f5612e5da26888d0bbec5
Ilyas Almakaev <i.almakaev@gmail.com>
2017-11-17 18:17:03 +0700

    open first view controller

---
+++ BrowserVK/modules/UserSearch/View/AIUserTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AIUserTableViewCell.swift
+//  BrowserVK
+//
+//  Created by Ильяс on 17.11.2017.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+import UIKit
+
+class AIUserTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
