13ca54662d308e5c764b666b8c5ce0c5b6060d7f
Ilyas Almakaev <i.almakaev@gmail.com>
2017-11-20 11:07:11 +0700

    configurate UserInfoCell and ViewController

---
+++ BrowserVK/modules/UserInfo/View/Cell/UserInfoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfoTableViewCell.swift
+//  BrowserVK
+//
+//  Created by Ильяс on 20.11.2017.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
